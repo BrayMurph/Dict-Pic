17a1330ce7051f0f69fcd29dbc8b842cfbf18d46
BrayMurph <brayxmurph@gmail.com>
2023-05-25 19:29:40 -0400

    the apis are now working as intended in the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,23 @@
 // Words API key with JSON call
-unirest.get("https://wordsapiv1.p.mashape.com/words/soliloquy")
-.header("X-Mashape-Key", "1ca162c33fmsha588cae174d0a24p1028f3jsnc1881d0ea824")
-.header("Accept", "application/json")
-.end(function (result) {
-  console.log(result.status, result.headers, result.body);
-});
+var wordApiRequest = "https://wordsapiv1.p.rapidapi.com/words/soliloquy";
+
+fetch(wordApiRequest, {
+  method: "GET",
+  credentials: "include",
+  headers: {
+    "Content-Type": "application/json",
+    "X-Mashape-Key": "1ca162c33fmsha588cae174d0a24p1028f3jsnc1881d0ea824"
+  }
+}).then(function (response) {
+  return response.json();
+  })
+  .then(function (data) {
+    console.log('Fetch Word API Response \n-------------');
+    console.log(data);
+  });
+
+//unirest.get("https://wordsapiv1.p.mashape.com/words/soliloquy")
+//.header("X-Mashape-Key", "1ca162c33fmsha588cae174d0a24p1028f3jsnc1881d0ea824")
 
 // GIPHY API key with JSON call
 var giphyUrl = "https://api.giphy.com/v1/gifs/search?api_key=sHdrKgH0F2FvUch4VPlNpZXnhV4IHZTI&q=Soup&limit=25&offset=0&rating=g&lang=en";
@@ -14,6 +27,6 @@ fetch(giphyUrl)
     return response.json();
   })
   .then(function (data) {
-    console.log('Fetch Response \n-------------');
+    console.log('Fetch Giphy API Response \n-------------');
     console.log(data);
   });
