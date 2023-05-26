4b5731635cf788b1ebc5734ee1455c863917b3e9
BrayMurph <brayxmurph@gmail.com>
2023-05-25 21:19:21 -0400

    made a function to add a word to the end of the url and fetch for that word

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,8 @@
 // Words API key with JSON call
-var wordApiRequest = "https://wordsapiv1.p.rapidapi.com/words/soliloquy";
+var fetchButton = document.querySelector(".searchButton");
 
-fetch(wordApiRequest, {
+function wordApi(wordUrl) {
+  fetch(wordUrl, {
   method: "GET",
   credentials: "include",
   headers: {
@@ -15,9 +16,28 @@ fetch(wordApiRequest, {
     console.log('Fetch Word API Response \n-------------');
     console.log(data);
   });
+}
 
-//unirest.get("https://wordsapiv1.p.mashape.com/words/soliloquy")
-//.header("X-Mashape-Key", "1ca162c33fmsha588cae174d0a24p1028f3jsnc1881d0ea824")
+function handleSearchFormSubmit (event) {
+  event.preventDefault();
+
+  var searchInputVal = document.querySelector(".searchWord").value;
+  var wordUrl = "https://wordsapiv1.p.rapidapi.com/words/";
+
+  
+  if(!searchInputVal) {
+    console.error("You need to enter a word!");
+    return;
+  }
+
+  wordUrl = wordUrl + searchInputVal;
+
+  wordApi(wordUrl);
+
+}
+
+fetchButton.addEventListener("click", handleSearchFormSubmit);
+
 
 // GIPHY API key with JSON call
 var giphyUrl = "https://api.giphy.com/v1/gifs/search?api_key=sHdrKgH0F2FvUch4VPlNpZXnhV4IHZTI&q=Soup&limit=25&offset=0&rating=g&lang=en";
