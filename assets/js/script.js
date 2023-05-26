af17a0644e3e21c4eb744904e172941b26230550
4FunkE <4FunkEcodes@gmail.com>
2023-05-25 20:48:20 -0400

    trying to load giphs to webpage, debugging

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,29 +16,36 @@ fetch(wordApiRequest, {
     console.log(data);
   });
 
-//unirest.get("https://wordsapiv1.p.mashape.com/words/soliloquy")
-//.header("X-Mashape-Key", "1ca162c33fmsha588cae174d0a24p1028f3jsnc1881d0ea824")
-
 // GIPHY API key with JSON call
-var giphyUrl = "https://api.giphy.com/v1/gifs/search?api_key=sHdrKgH0F2FvUch4VPlNpZXnhV4IHZTI&q=Soup&limit=25&offset=0&rating=g&lang=en";
+  var giphyUrl = "https://api.giphy.com/v1/gifs/search?api_key=sHdrKgH0F2FvUch4VPlNpZXnhV4IHZTI&q=Soup&limit=25&offset=0&rating=g&lang=en";
 
-fetch(giphyUrl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log('Fetch Giphy API Response \n-------------');
-    console.log(data);
+  fetch(giphyUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log('Fetch Giphy API Response \n-------------');
+      console.log(data);
   });
 
+function loadGiphys() {
+  var giphyPlace = new XMLHttpRequest();
+  giphyPlace.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      var data = JSON.parse(this.responseText);
+      document.getElementById("gifs").src = data.results[0].picture.large
+    }
+  };
+  giphyPlace.open("GET", "https://api.giphy.com/v1/gifs/search?api_key=sHdrKgH0F2FvUch4VPlNpZXnhV4IHZTI&q=Soup&limit=25&offset=0&rating=g&lang=en", true);
+  giphyPlace.send();
+}
 
 //Define button function 
 $(function() {
   var defineBtn = $(".searchButton");
 
-  defineBtn.on("click", function () {
-    //call unirest API for defintion
-    //convert to text in html document
+  defineBtn.on("click", function() {
+    console.log("clicked!")
   })
 });
 
