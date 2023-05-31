a101aceb8dda4bda16dc911559302ea99ad36800
4FunkE <4FunkEcodes@gmail.com>
2023-05-31 19:16:31 -0400

    debuging loading giphy

--- assets/js/script.js
+++ assets/js/script.js
@@ -54,15 +54,49 @@ function gifApi(gifUrl) {
     .then(function (data) {
       console.log('Fetch Giphy API Response \n-------------');
       console.log(data);
-      for (var i = 0; i < data.length; i++) {
-        var gif = document.createElement('h3');
-        gif.textContent = data[i].embed_url;
-        issueContainer.append(gif);
-      }
+
+    //You should be able to find a giph/img url somewhere in the returned response which 
+    //you can use javascript to set as the source to an image in your html.
+      //for (var i = 0; i < data.length; i++) {
+        //var gif = document.createElement('h3');
+        //gif.textContent = data[i].embed_url;
+        //issueContainer.append(gif);
+      //}
   });
 };
-
 //fetchButton.addEventListener("click", gifApi);
+
+function loadGiphys() {
+  var figure = document.createElement("figure");
+  var img = document.createElement("img");
+  var caption = document.createElement("figurecaption");
+
+  img.src = content.data[0].embed_url;
+  img.alt = content.data[0].title;
+      
+  caption.textContent = content.data[0].title;
+      figure.appendChild(img);
+      figure.appendChild(caption);
+  let populate = document .querySelector(".gifs");
+      populate.insertAdjacentElement("afterbegin", figure);
+};
+fetchButton.addEventListener("click", loadGiphys);
+
+// populate gif to webpage
+//function loadGiphys() {
+  //var giphyPlace = new XMLHttpRequest();
+  //giphyPlace.onreadystatechange = function() {
+    //if (this.readyState == 4 && this.status == 200) {
+      //var data = JSON.parse(this.responseText);
+      //document.getElementById("gifs").src = data[0].picture.large
+    //}
+  //};
+  //giphyPlace.open("GET", "https://api.giphy.com/v1/gifs/search?api_key=sHdrKgH0F2FvUch4VPlNpZXnhV4IHZTI&q=Soup&limit=25&offset=0&rating=g&lang=en", true);
+  //giphyPlace.send();
+//}
+//fetchButton.addEventListener("click", loadGiphys);
+
+
 
 //Define button function 
 $(function() {
